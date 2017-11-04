68b82777b77bb72d986ecdc8a3fc6aadb953d2ee
gustavopimentelrinaldi <gustavorinaldiiii@gmail.com>
2017-11-04 16:14:50 -0200

    Added paperclip

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,4 +1,6 @@
 class Pic < ApplicationRecord
   belongs_to :user
+  has_attached_file :image, :styles => { :medium => "300x300>" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates :title, :description, presence: true
 end
